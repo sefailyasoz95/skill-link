58dfc622f2ce0cd7bd76e266754512008169adae
Sefa İlyas Öz <sefailyas1455@gmail.com>
2025-05-19 12:48:17 +0300

    updates

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -1,195 +1,162 @@
 "use client";
 
-import {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  ReactNode,
-} from "react";
+import { createContext, useContext, useState, useEffect, ReactNode } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { Session } from "@supabase/supabase-js";
 import { supabase } from "@/lib/supabase-client";
 import { User } from "@/lib/types";
 
 type AuthContextType = {
-  user: User | null;
-  session: Session | null;
-  signUp: (email: string, password: string) => Promise<{ error: any }>;
-  signIn: (
-    email: string,
-    password: string,
-    redirectTo?: string
-  ) => Promise<{ error: any }>;
-  signOut: () => Promise<void>;
-  loading: boolean;
-  googleSignIn: (redirectTo?: string) => Promise<{ error: any }>;
+	user: User | null;
+	session: Session | null;
+	signUp: (email: string, password: string) => Promise<{ error: any }>;
+	signIn: (email: string, password: string, redirectTo?: string) => Promise<{ error: any }>;
+	signOut: () => Promise<void>;
+	loading: boolean;
+	googleSignIn: (redirectTo?: string) => Promise<{ error: any }>;
 };
 
 const AuthContext = createContext<AuthContextType>({
-  user: null,
-  session: null,
-  signUp: async () => ({ error: null }),
-  signIn: async () => ({ error: null }),
-  signOut: async () => {},
-  loading: true,
-  googleSignIn: async () => ({ error: null }),
+	user: null,
+	session: null,
+	signUp: async () => ({ error: null }),
+	signIn: async () => ({ error: null }),
+	signOut: async () => {},
+	loading: true,
+	googleSignIn: async () => ({ error: null }),
 });
 
 export const useAuth = () => useContext(AuthContext);
 
 type AuthProviderProps = {
-  children: ReactNode;
-  redirectIfUnauthenticated?: boolean;
+	children: ReactNode;
+	redirectIfUnauthenticated?: boolean;
 };
 
-const PUBLIC_ROUTES = [
-  "/",
-  "/auth/signin",
-  "/auth/callback",
-  "/how-it-works",
-  "/auth/signup",
-];
+const PUBLIC_ROUTES = ["/", "/auth/signin", "/auth/callback", "/how-it-works", "/auth/signup", "/privacy-and-terms"];
 
-const AuthProvider = ({
-  children,
-  redirectIfUnauthenticated = true,
-}: AuthProviderProps) => {
-  const [user, setUser] = useState<User | null>(null);
-  const [session, setSession] = useState<Session | null>(null);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  const pathname = usePathname();
+const AuthProvider = ({ children, redirectIfUnauthenticated = true }: AuthProviderProps) => {
+	const [user, setUser] = useState<User | null>(null);
+	const [session, setSession] = useState<Session | null>(null);
+	const [loading, setLoading] = useState(true);
+	const router = useRouter();
+	const pathname = usePathname();
 
-  useEffect(() => {
-    const getSession = async () => {
-      try {
-        const { data, error } = await supabase.auth.getSession();
-        setSession(data.session);
-        if (!data.session) {
-          setUser(null);
-        } else {
-          const { data: userData } = await supabase
-            .from("users")
-            .select("*")
-            .eq("id", data.session.user.id)
-            .single();
-          setUser(userData ?? null);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-    getSession();
+	useEffect(() => {
+		const getSession = async () => {
+			try {
+				const { data, error } = await supabase.auth.getSession();
+				setSession(data.session);
+				if (!data.session) {
+					setUser(null);
+				} else {
+					const { data: userData } = await supabase.from("users").select("*").eq("id", data.session.user.id).single();
+					setUser(userData ?? null);
+				}
+			} finally {
+				setLoading(false);
+			}
+		};
+		getSession();
 
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange(async (_event, newSession) => {
-      setSession(newSession);
-      if (!newSession) {
-        setUser(null);
-      } else {
-        const { data: userData } = await supabase
-          .from("users")
-          .select("*")
-          .eq("id", newSession.user.id)
-          .single();
-        setUser(userData ?? null);
-      }
-      setLoading(false);
-    });
-    return () => subscription.unsubscribe();
-  }, []);
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange(async (_event, newSession) => {
+			setSession(newSession);
+			if (!newSession) {
+				setUser(null);
+			} else {
+				const { data: userData } = await supabase.from("users").select("*").eq("id", newSession.user.id).single();
+				setUser(userData ?? null);
+			}
+			setLoading(false);
+		});
+		return () => subscription.unsubscribe();
+	}, []);
 
-  // Only redirect if not loading, not on a public route, and user is not authenticated
-  useEffect(() => {
-    if (
-      !loading &&
-      !user &&
-      redirectIfUnauthenticated &&
-      !PUBLIC_ROUTES.some(
-        (route) => pathname === route || pathname.startsWith(route + "/")
-      )
-    ) {
-      router.replace("/auth/signin?redirectTo=" + encodeURIComponent(pathname));
-    }
-  }, [user, loading, pathname, redirectIfUnauthenticated, router]);
+	// Only redirect if not loading, not on a public route, and user is not authenticated
+	useEffect(() => {
+		if (
+			!loading &&
+			!user &&
+			redirectIfUnauthenticated &&
+			!PUBLIC_ROUTES.some((route) => pathname === route || pathname.startsWith(route + "/"))
+		) {
+			router.replace("/auth/signin?redirectTo=" + encodeURIComponent(pathname));
+		}
+	}, [user, loading, pathname, redirectIfUnauthenticated, router]);
 
-  const signUp = async (email: string, password: string) => {
-    try {
-      const { error } = await supabase.auth.signUp({
-        email,
-        password,
-        options: {
-          emailRedirectTo: `${window.location.origin}/auth/callback`,
-        },
-      });
-      return { error };
-    } catch (error) {
-      console.error("Error in signUp:", error);
-      return { error };
-    }
-  };
+	const signUp = async (email: string, password: string) => {
+		try {
+			const { error } = await supabase.auth.signUp({
+				email,
+				password,
+				options: {
+					emailRedirectTo: `${window.location.origin}/auth/callback`,
+				},
+			});
+			return { error };
+		} catch (error) {
+			console.error("Error in signUp:", error);
+			return { error };
+		}
+	};
 
-  const signIn = async (
-    email: string,
-    password: string,
-    redirectTo?: string
-  ) => {
-    try {
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-      if (!error && redirectTo) {
-        router.push(redirectTo);
-      }
-      return { error };
-    } catch (error) {
-      console.error("Error in signIn:", error);
-      return { error };
-    }
-  };
+	const signIn = async (email: string, password: string, redirectTo?: string) => {
+		try {
+			const { error } = await supabase.auth.signInWithPassword({
+				email,
+				password,
+			});
+			if (!error && redirectTo) {
+				router.push(redirectTo);
+			}
+			return { error };
+		} catch (error) {
+			console.error("Error in signIn:", error);
+			return { error };
+		}
+	};
 
-  const signOut = async () => {
-    try {
-      await supabase.auth.signOut();
-      setUser(null);
-      setSession(null);
-      router.push("/");
-    } catch (error) {
-      console.error("Error in signOut:", error);
-    }
-  };
+	const signOut = async () => {
+		try {
+			await supabase.auth.signOut();
+			setUser(null);
+			setSession(null);
+			router.push("/");
+		} catch (error) {
+			console.error("Error in signOut:", error);
+		}
+	};
 
-  const googleSignIn = async (redirectTo?: string) => {
-    try {
-      if (redirectTo) {
-        sessionStorage.setItem("redirectAfterAuth", redirectTo);
-      }
-      const { error } = await supabase.auth.signInWithOAuth({
-        provider: "google",
-        options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
-        },
-      });
-      return { error };
-    } catch (error) {
-      console.error("Error in googleSignIn:", error);
-      return { error };
-    }
-  };
+	const googleSignIn = async (redirectTo?: string) => {
+		try {
+			if (redirectTo) {
+				sessionStorage.setItem("redirectAfterAuth", redirectTo);
+			}
+			const { error } = await supabase.auth.signInWithOAuth({
+				provider: "google",
+				options: {
+					redirectTo: `${window.location.origin}/auth/callback`,
+				},
+			});
+			return { error };
+		} catch (error) {
+			console.error("Error in googleSignIn:", error);
+			return { error };
+		}
+	};
 
-  const value = {
-    user,
-    session,
-    signUp,
-    signIn,
-    signOut,
-    loading,
-    googleSignIn,
-  };
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+	const value = {
+		user,
+		session,
+		signUp,
+		signIn,
+		signOut,
+		loading,
+		googleSignIn,
+	};
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
--- components/header.tsx
+++ components/header.tsx
@@ -6,200 +6,166 @@ import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import {
-  Menu,
-  X,
-  Search,
-  MessageSquare,
-  User,
-  LayoutDashboard,
-} from "lucide-react";
+import { Menu, X, Search, MessageSquare, User, LayoutDashboard } from "lucide-react";
 import { trackEvent } from "@/lib/firebase";
 import { useAuth } from "./auth-provider";
 import Image from "next/image";
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const pathname = usePathname();
-  const { user, signOut } = useAuth();
+	const [isOpen, setIsOpen] = useState(false);
+	const pathname = usePathname();
+	const { user, signOut } = useAuth();
 
-  useEffect(() => {
-    trackEvent("home_page_viewed", {
-      event_category: "home_page",
-      event_label: "home_page_viewed",
-      non_interaction: true,
-    });
-  }, []);
-  const routes = [
-    // {
-    // 	name: "Home",
-    // 	path: "/",
-    // },
-    // {
-    // 	name: "Search",
-    // 	path: "/search",
-    // },
-    {
-      name: "How It Works",
-      path: "/how-it-works",
-    },
-  ];
+	useEffect(() => {
+		trackEvent("home_page_viewed", {
+			event_category: "home_page",
+			event_label: "home_page_viewed",
+			non_interaction: true,
+		});
+	}, []);
+	const routes = [
+		// {
+		// 	name: "Home",
+		// 	path: "/",
+		// },
+		// {
+		// 	name: "Search",
+		// 	path: "/search",
+		// },
+		{
+			name: "How It Works",
+			path: "/how-it-works",
+		},
+	];
 
-  const authenticatedRoutes = [
-    {
-      name: "Dashboard",
-      path: "/dashboard",
-      icon: <LayoutDashboard className="mr-2 h-4 w-4" />,
-    },
-    {
-      name: "Messages",
-      path: "/messages",
-      icon: <MessageSquare className="mr-2 h-4 w-4" />,
-    },
-    {
-      name: "Profile",
-      path: "/profile",
-      icon: <User className="mr-2 h-4 w-4" />,
-    },
-  ];
+	const authenticatedRoutes = [
+		{
+			name: "Dashboard",
+			path: "/dashboard",
+			icon: <LayoutDashboard className='mr-2 h-4 w-4' />,
+		},
+		{
+			name: "Messages",
+			path: "/messages",
+			icon: <MessageSquare className='mr-2 h-4 w-4' />,
+		},
+		{
+			name: "Profile",
+			path: "/profile",
+			icon: <User className='mr-2 h-4 w-4' />,
+		},
+	];
 
-  return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <div className="flex gap-6 md:gap-10">
-          <Link
-            href={user ? "/dashboard" : "/"}
-            className="items-center space-x-2 flex"
-          >
-            <Image
-              src={"/skill_link_logo.png"}
-              alt="Skill Link"
-              width={75}
-              height={75}
-            />
-          </Link>
-          {!user && (
-            <nav className="hidden md:flex gap-6">
-              {routes.map((route) => (
-                <Link
-                  key={route.path}
-                  href={route.path}
-                  className={`text-sm my-auto font-medium transition-colors hover:text-primary ${
-                    pathname === route.path
-                      ? "text-foreground"
-                      : "text-muted-foreground"
-                  }`}
-                >
-                  {route.name}
-                </Link>
-              ))}
-            </nav>
-          )}
-        </div>
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="hidden md:flex items-center space-x-2">
-            <ModeToggle />
-            {user ? (
-              <>
-                <Button variant="ghost" size="icon" asChild>
-                  <Link href="/search">
-                    <Search className="h-4 w-4" />
-                    <span className="sr-only">Search</span>
-                  </Link>
-                </Button>
-                <Button variant="ghost" size="icon" asChild>
-                  <Link href="/messages">
-                    <MessageSquare className="h-4 w-4" />
-                    <span className="sr-only">Messages</span>
-                  </Link>
-                </Button>
-                <Button variant="ghost" size="sm" asChild>
-                  <Link href="/dashboard">Dashboard</Link>
-                </Button>
-                <Button variant="outline" size="sm" onClick={signOut}>
-                  Sign Out
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button variant="ghost" size="sm" asChild>
-                  <Link href="/auth/signin">Sign In</Link>
-                </Button>
-              </>
-            )}
-          </nav>
-          <Sheet open={isOpen} onOpenChange={setIsOpen}>
-            <SheetTrigger asChild>
-              <Button
-                variant="ghost"
-                className="md:hidden"
-                size="icon"
-                aria-label="Toggle Menu"
-              >
-                <Menu className="h-5 w-5" />
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="right" className="pr-0" title="Skill Link">
-              <Link
-                href="/"
-                className="flex items-center"
-                onClick={() => setIsOpen(false)}
-              >
-                <span className="font-bold">Skill Link</span>
-              </Link>
-              <div className="mt-8 flex flex-col space-y-4">
-                {routes.map((route) => (
-                  <Link
-                    key={route.path}
-                    href={route.path}
-                    className="flex w-full items-center py-2 text-sm font-medium transition-colors hover:text-primary"
-                    onClick={() => setIsOpen(false)}
-                  >
-                    {route.name}
-                  </Link>
-                ))}
-                <div className="h-px bg-muted my-4" />
-                {user ? (
-                  <>
-                    {authenticatedRoutes.map((route) => (
-                      <Link
-                        key={route.path}
-                        href={route.path}
-                        className="flex w-full items-center py-2 text-sm font-medium transition-colors hover:text-primary"
-                        onClick={() => setIsOpen(false)}
-                      >
-                        {route.icon}
-                        {route.name}
-                      </Link>
-                    ))}
-                    <Button
-                      variant="outline"
-                      className="mt-4"
-                      onClick={signOut}
-                    >
-                      Sign Out
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <Link
-                      href="/auth/signin"
-                      className="flex w-full items-center py-2 text-sm font-medium transition-colors hover:text-primary"
-                      onClick={() => setIsOpen(false)}
-                    >
-                      Sign In
-                    </Link>
-                  </>
-                )}
-                <div className="flex items-center justify-between py-4">
-                  <span className="text-sm font-medium">Toggle Theme</span>
-                  <ModeToggle />
-                </div>
-              </div>
-            </SheetContent>
-          </Sheet>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className='sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+			<div className='container mx-auto flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0'>
+				<div className='flex gap-6 md:gap-10'>
+					<Link href={user ? "/dashboard" : "/"} className='items-center space-x-2 flex'>
+						<span className='hidden font-bold sm:inline-block text-xl'>Skill Link</span>
+					</Link>
+					{!user && (
+						<nav className='hidden md:flex gap-6'>
+							{routes.map((route) => (
+								<Link
+									key={route.path}
+									href={route.path}
+									className={`text-sm my-auto font-medium transition-colors hover:text-primary ${
+										pathname === route.path ? "text-foreground" : "text-muted-foreground"
+									}`}>
+									{route.name}
+								</Link>
+							))}
+						</nav>
+					)}
+				</div>
+				<div className='flex flex-1 items-center justify-end space-x-4'>
+					<nav className='hidden md:flex items-center space-x-2'>
+						<ModeToggle />
+						{user ? (
+							<>
+								<Button variant='ghost' size='icon' asChild>
+									<Link href='/search'>
+										<Search className='h-4 w-4' />
+										<span className='sr-only'>Search</span>
+									</Link>
+								</Button>
+								<Button variant='ghost' size='icon' asChild>
+									<Link href='/messages'>
+										<MessageSquare className='h-4 w-4' />
+										<span className='sr-only'>Messages</span>
+									</Link>
+								</Button>
+								<Button variant='ghost' size='sm' asChild>
+									<Link href='/dashboard'>Dashboard</Link>
+								</Button>
+								<Button variant='outline' size='sm' onClick={signOut}>
+									Sign Out
+								</Button>
+							</>
+						) : (
+							<>
+								<Button variant='ghost' size='sm' asChild>
+									<Link href='/auth/signin'>Sign In</Link>
+								</Button>
+							</>
+						)}
+					</nav>
+					<Sheet open={isOpen} onOpenChange={setIsOpen}>
+						<SheetTrigger asChild>
+							<Button variant='ghost' className='md:hidden' size='icon' aria-label='Toggle Menu'>
+								<Menu className='h-5 w-5' />
+							</Button>
+						</SheetTrigger>
+						<SheetContent side='right' className='pr-0' title='Skill Link'>
+							<Link href='/' className='flex items-center' onClick={() => setIsOpen(false)}>
+								<span className='font-bold'>Skill Link</span>
+							</Link>
+							<div className='mt-8 flex flex-col space-y-4'>
+								{routes.map((route) => (
+									<Link
+										key={route.path}
+										href={route.path}
+										className='flex w-full items-center py-2 text-sm font-medium transition-colors hover:text-primary'
+										onClick={() => setIsOpen(false)}>
+										{route.name}
+									</Link>
+								))}
+								<div className='h-px bg-muted my-4' />
+								{user ? (
+									<>
+										{authenticatedRoutes.map((route) => (
+											<Link
+												key={route.path}
+												href={route.path}
+												className='flex w-full items-center py-2 text-sm font-medium transition-colors hover:text-primary'
+												onClick={() => setIsOpen(false)}>
+												{route.icon}
+												{route.name}
+											</Link>
+										))}
+										<Button variant='outline' className='mt-4' onClick={signOut}>
+											Sign Out
+										</Button>
+									</>
+								) : (
+									<>
+										<Link
+											href='/auth/signin'
+											className='flex w-full items-center py-2 text-sm font-medium transition-colors hover:text-primary'
+											onClick={() => setIsOpen(false)}>
+											Sign In
+										</Link>
+									</>
+								)}
+								<div className='flex items-center justify-between py-4'>
+									<span className='text-sm font-medium'>Toggle Theme</span>
+									<ModeToggle />
+								</div>
+							</div>
+						</SheetContent>
+					</Sheet>
+				</div>
+			</div>
+		</header>
+	);
 }
